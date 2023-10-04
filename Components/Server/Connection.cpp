485b9d9f933332d2784493904f9dc02407c05f54
beadam <adam.ben.yahia.2001@gmail.com>
2023-10-04 23:39:51 +0100

    here comes the sun tudododooo

--- Components/Server/Connection.cpp
+++ Components/Server/Connection.cpp
@@ -87,13 +87,7 @@ void Server::requests(int indexClient)
             clientDisconnected(this->pfds[indexClient].fd);
         else
             runCommand(this->pfds[indexClient].fd, joinBuffers(indexClient, buffer));
-        std::cout << "buffer: "<< buffer << std::endl;
-    }
-    if (!this->responses.empty())
-    {
-        std::pair<int, std::string> response = this->responses.front();
-        this->responses.pop();
-        send(response.first, response.second.c_str(), response.second.length(), 0);
+        std::cout << "buffer: " << buffer << std::endl;
     }
 }
 
@@ -121,12 +115,17 @@ void Server::multipleClients()
         int numfds = poll(&(this->pfds[0]), this->pfds.size(), POLL_TIMEOUT);
         if (numfds == -1)
             customException("Error : poll failed");
-        if (numfds == 0)
-            continue;
         if (this->pfds[0].revents & POLLIN)
             acceptAndDecline();
-        else
+        else if (numfds > 0)
             for (size_t i = 0; i < this->pfds.size(); i++)
                 requests(i);
+        if (!this->responses.empty())
+        {
+            std::pair<int, std::string> response = this->responses.front();
+            this->responses.pop();
+            send(response.first, response.second.c_str(), response.second.length(), 0);
+            std::cout << "count :" << this->responses.size() << std::endl;
+        }
     }
 }
