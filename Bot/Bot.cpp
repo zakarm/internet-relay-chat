1648999f90810cd84d41bb3e634564f4bfb468e1
zakarm <zakariaemrabet1@gmail.com>
2023-10-05 16:16:04 +0100

    etape 2 bot connection done

--- Bot/Bot.cpp
+++ Bot/Bot.cpp
@@ -23,6 +23,10 @@ Bot::~Bot()
     close(this->clientSocket);
 }
 
+/**************************************************************/
+/*                         Functions                          */
+/**************************************************************/
+
 void Bot::connectToServer()
 {
     if (connect(this->clientSocket, (struct sockaddr *)&this->clientAddr, this->socketLen) < 0)
@@ -37,12 +41,33 @@ void Bot::connectToServer()
         char buffer[1024];
         bzero(buffer, sizeof(buffer));
         int err = recv(this->clientSocket, buffer, sizeof(buffer), 0);
-        if (err != -1)
-            std::cout << buffer;
+        if (err > 0)
+            runCommand(clientSocket, buffer);
         else
         {
             close(this->clientSocket);
             exit(EXIT_FAILURE);
         }
+    }
+}
+
+void Bot::runCommand(int clientSocket, std::string data)
+{
+    (void)clientSocket;
+    std::stringstream ss(data);
+    std::string param, command, client, message;
+    ss >> param;
+    ss >> std::ws;
+    ss >> command;
+    ss >> std::ws;
+    ss >> client;
+    ss >> std::ws;
+    std::getline(ss, message);
+    if (message == ":time\r\n" || message == "time\r\n" || message == ":time\n" || message == ":time\r"
+    || message == "time\n" || message == "time\r")
+    {
+        std::stringstream ss;
+        ss << "PRIVMSG " << param.substr(1, param.find('!') - 1).c_str() << " :time " << Utils::getDate() << "\r\n";
+        send(this->clientSocket, ss.str().c_str(), ss.str().size(), 0);
     }
 }
