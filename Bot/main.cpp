85df34aacf63bf64d3f2922cfd4c4307b962b0a5
Zakariae Mrabet <zmrabet@e3r4p10.1337.ma>
2023-10-12 23:36:26 +0100

    bot protections

--- Bot/main.cpp
+++ Bot/main.cpp
@@ -5,11 +5,15 @@ int main (int argc, char **argv)
     try
     {
         if (argc != 4)
+        {
+            std::cerr << RED << "Error: Invalid args" << DEFAULT << std::endl;
             exit(0);
+        }
+        port_check(argv[1]);
         Bot bot(atoi(argv[1]), argv[2], argv[3]);
     }
     catch(std::exception &ex)
     {
-        std::cerr << RED << "Error : " << ex.what() << DEFAULT << std::endl;
+        std::cerr << RED << ex.what() << DEFAULT << std::endl;
     }
 }
